07993d2e0e25e6e342676da35b40c5782f68e2af
João Medeiros <jppaulo@outlook.com>
2022-11-11 20:04:40 -0300

    Constructor

--- ByteBank/1-ByteBank/Program.cs
+++ ByteBank/1-ByteBank/Program.cs
@@ -5,8 +5,8 @@
         private static void Main(string[] args)
         {
             Console.WriteLine("ByteBank");
-            Cliente cliente = new Cliente(nome:"João", cpf:"123.456.789-00", profissao:"Um qualquer");
-            ContaCorrente cc = new ContaCorrente(titular:cliente, agencia: 123, numero: 556677, 1037.80);
+            Cliente cliente = new(nome:"João", cpf:"123.456.789-00", profissao:"Um qualquer");
+            ContaCorrente cc = new(titular:cliente, agencia: 123, numero: 556677, 1037.80);
 
             Console.WriteLine(cc.Titular.Nome);
             Console.WriteLine(cc.Titular.CPF);
