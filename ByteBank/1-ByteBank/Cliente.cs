5de53faf3339235e833edb7ba1618d1feedee4b5
João Medeiros <jppaulo@outlook.com>
2022-11-11 09:03:28 -0300

    Create Cliente class.

---
+++ ByteBank/1-ByteBank/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+
+        //public int agencia;
+        //public int numero;
+        //public double saldo;
+        public Cliente(string nome, string cpf, string profissao)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.profissao= profissao;
+        }
+    }
+}
--- ByteBank/1-ByteBank/Program.cs
+++ ByteBank/1-ByteBank/Program.cs
@@ -1,10 +1,21 @@
-﻿internal class Program
+﻿namespace _1_ByteBank
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("ByteBank");
-        ContaCorrente cc = new ContaCorrente();
-        Console.WriteLine(cc);
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("ByteBank");
+            Cliente cliente = new Cliente(nome:"João", cpf:"123.456.789-00", profissao:"Um qualquer");
+            ContaCorrente cc = new ContaCorrente(cliente: cliente, agencia: 123, numero: 556677, saldo: 123.75);
 
+            Console.WriteLine(cc.cliente.nome);
+            Console.WriteLine(cc.cliente.cpf);
+            Console.WriteLine(cc.cliente.profissao);
+            Console.WriteLine(cc.agencia);
+            Console.WriteLine(cc.numero);
+            Console.WriteLine(cc.saldo);
+
+            Console.WriteLine(cc);
+        }
     }
 }
