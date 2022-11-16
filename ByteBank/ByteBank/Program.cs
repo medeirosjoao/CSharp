6713cbc706fce3041a99470507194f2e6e6f9688
João Medeiros <jppaulo@outlook.com>
2022-11-16 14:28:02 -0300

    Herança, Polimorfismo e Modificadores

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ByteBank;
+using ByteBank.Funcionarios;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+        Funcionario carlos = new Funcionario();       
+        carlos.Nome = "Carlos";
+        carlos.CPF = "123.456.789.00";
+        carlos.Salario = 2000;
+        gerenciador.Registrar(carlos);
+
+        Diretor roberta = new Diretor();
+        roberta.Nome = "Roberta";
+        roberta.CPF = "321.345.123.98";
+        roberta.Salario = 5000;
+        gerenciador.Registrar(roberta);
+
+        Console.WriteLine(carlos.Nome);
+        Console.WriteLine(carlos.GetBonificacao());
+
+        Console.WriteLine(roberta.Nome);
+        Console.WriteLine(roberta.GetBonificacao());
+        Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+    }
+}
