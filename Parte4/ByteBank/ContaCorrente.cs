e03934f79491a7c067559cfe81b83cb656a62e69
João Medeiros <jppaulo@outlook.com>
2022-11-23 11:16:41 -0300

    Exceptions

--- Parte4/ByteBank/ContaCorrente.cs
+++ Parte4/ByteBank/ContaCorrente.cs
@@ -8,14 +8,13 @@ namespace ByteBank
 {
     public class ContaCorrente
     {
-        public static double TaxaOperacao { get; private set; }
+        private static int TaxaOperacao;
 
         public static int TotalDeContasCriadas { get; private set; }
 
         public Cliente Titular { get; set; }
 
         public int Numero { get; }
-
         public int Agencia { get; }
 
         private double _saldo = 100;
@@ -38,33 +37,36 @@ namespace ByteBank
 
         public ContaCorrente(int agencia, int numero)
         {
-            if (agencia <= 0)
-            {
-                throw new ArgumentException("O Argumento agência deve ser maior que 0", nameof(agencia));
-                //Console.WriteLine(Titular.Nome);
-            }
             if (numero <= 0)
             {
-                throw new ArgumentException("O Argumento numero deve ser maior que  0", nameof(numero));
-                //Console.WriteLine(Titular.Nome);
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
             }
+
+            if(numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
             Agencia = agencia;
             Numero = numero;
+
             TotalDeContasCriadas++;
-
             TaxaOperacao = 30 / TotalDeContasCriadas;
-
         }
 
-        public bool Sacar(double valor)
+        public void Sacar(double valor)
         {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
+            }
+
             if (_saldo < valor)
             {
-                return false;
+                throw new SaldoInsuficienteException(Saldo, valor);
             }
 
             _saldo -= valor;
-            return true;
         }
 
         public void Depositar(double valor)
@@ -72,16 +74,15 @@ namespace ByteBank
             _saldo += valor;
         }
 
-        public bool Transferir(double valor, ContaCorrente contaDestino)
+        public void Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (_saldo < valor)
+            if (valor < 0)
             {
-                return false;
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
             }
 
-            _saldo -= valor;
+            Sacar(valor);
             contaDestino.Depositar(valor);
-            return true;
         }
     }
 }
--- Parte4/ByteBank/Program.cs
+++ Parte4/ByteBank/Program.cs
@@ -12,18 +12,40 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta = new ContaCorrente(0, 0);
+                ContaCorrente conta = new ContaCorrente(456, 4578420);
+                ContaCorrente conta2 = new ContaCorrente(485, 456478);
+
+                conta2.Transferir(-10, conta);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(-500);
+                Console.WriteLine(conta.Saldo);
             }
             catch (ArgumentException ex)
             {
-               //Console.WriteLine("Erro no parâmetro " + ex.ParamName);
-                Console.WriteLine("ex.Message " + ex.Message);
+                if(ex.ParamName == "numero")
+                {
+                    
+                }
+
+                Console.WriteLine("Argumento com problema: " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch(SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
             }
             catch (Exception ex)
             {
-               // Console.WriteLine("ex.Message " + ex.Message);
+                Console.WriteLine(ex.Message);
             }
+            
+            //Metodo();
 
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
         }
 
@@ -50,7 +72,9 @@ namespace ByteBank
             {
                 Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
                 throw;
+                Console.WriteLine("Código depois do throw");
             }
         }
+
     }
 }
