d6b04e2601a77c676212bf3689790c3c1b0a889d
João Medeiros <jppaulo@outlook.com>
2022-11-24 10:42:26 -0300

    Modificadores Internal e Internal Protected

--- Parte5/ByteBank.Modelos/AutenticacaoHelper.cs
+++ Parte5/ByteBank.Modelos/AutenticacaoHelper.cs
@@ -8,5 +8,9 @@ namespace ByteBank.Modelos
 {
     internal class AutenticacaoHelper
     {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
     }
 }
