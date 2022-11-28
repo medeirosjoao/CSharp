27fa081070681c600823892161e6bed11813b2d9
João Medeiros <jppaulo@outlook.com>
2022-11-28 11:03:56 -0300

    IndexOf, IsNullOrEmpty, Length

---
+++ Parte6/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser vazio.", nameof(url));
+            }
+
+            URL = url;
+        }
+    }
+}
--- Parte6/ByteBank.SistemaAgencia/Program.cs
+++ Parte6/ByteBank.SistemaAgencia/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
-using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -13,11 +12,36 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string url = "pagina?argumentos";
-            string argumentos = url.Substring(7);
+            // pagina?argumentos
+            // 012345678
+
+            //string textoVazio = "";
+            //string textoNulo = null;
+            //string textoQualquer = "kjhfsdjhgsdfjksdf";
+
+
+            //Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            //Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            //Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            //Console.ReadLine();
+
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumentos);
+
 
             Console.ReadLine();
         }
     }
+
+
 }
