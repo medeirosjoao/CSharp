e069184d4051b630eb1a5a09358c8cb8e06c12de
João Medeiros <jppaulo@outlook.com>
2022-11-28 16:09:52 -0300

    Remove, Replace, ToUpper, Lower, Starts EndsWith

--- Parte6/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ Parte6/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -8,17 +8,42 @@ namespace ByteBank.SistemaAgencia
 {
     public class ExtratorValorDeArgumentosURL
     {
+        private readonly string _argumentos;
         public string URL { get; }
 
         public ExtratorValorDeArgumentosURL(string url)
         {
-
-            if (string.IsNullOrEmpty(url))
+            if(String.IsNullOrEmpty(url))
             {
-                throw new ArgumentException("O argumento url não pode ser vazio.", nameof(url));
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
             }
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
 
             URL = url;
         }
+
+        // moedaOrigem=real&moedaDestino=dolar
+        // MOEDAORIGEM=REAL&MOEDADESTINO=DOLAR
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper(); // VALOR
+            string argumentoEmCaixaAlta = _argumentos.ToUpper(); // MOEDAORIGEM=REAL&MOEDADESTINO=DOLAR
+
+            string termo = nomeParametro + "="; // moedaDestino=
+            int indiceTermo = argumentoEmCaixaAlta.IndexOf(termo); // x
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length); // dolar
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if(indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
+        }
+
     }
 }
