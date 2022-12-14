ff69131c5f94f45508b2df82bf711472b4325389
João Medeiros <jppaulo@outlook.com>
2022-12-14 09:42:47 -0300

    Aula 2
    Como enviar requisições para a API.
    Como preparar a API para receber requisições.
    A finalidade do verbo POST.
    Como criar um recurso no sistema.
    Como adicionar validações nos parâmetros enviados.

---
+++ API/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
