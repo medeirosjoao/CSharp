78d5e6826fcdd45de215aec2094dd3cfbdff37b9
João Medeiros <jppaulo@outlook.com>
2022-12-14 17:15:21 -0300

    Aula 03
    Como recuperar informações da API.
    A finalidade do verbo GET.
    Como enviar parâmetros através da URL de requisição.
    Como filtrar recursos para retornar para o usuário.
    Como devemos retornar as informações para o usuário com base no tipo de requisição.

--- API/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ API/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace FilmesAPI.Controllers
@@ -12,12 +13,38 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+            //foreach (Filme filme in filmes)
+            //{
+            //    if (filme.Id == id)
+            //        return filme;
+            //}
+            //return null;
         }
     }
 }
